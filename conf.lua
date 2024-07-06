764d328b5d335b5ed6022fa896ff217abd6cbf44
polypoyo <23138363+polypoyo@users.noreply.github.com>
2024-07-06 09:49:40 -0400

    Use external storage on Android

--- conf.lua
+++ conf.lua
@@ -20,6 +20,7 @@ function love.conf(t)
     t.window.icon = "icon.png"
     t.window.width = 640
     t.window.height = 480
+    t.externalstorage = true
 
     if major >= 12 then
         t.highdpi = true
